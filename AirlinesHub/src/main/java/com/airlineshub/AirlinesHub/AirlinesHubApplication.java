c873d223c149b168c57a597003a8757617597112
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2025-10-22 18:41:27 -0300

    Criando projetos com docker.

---
+++ AirlinesHub/src/main/java/com/airlineshub/AirlinesHub/AirlinesHubApplication.java
@@ -0,0 +1,13 @@
+package com.airlineshub.AirlinesHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinesHubApplication.class, args);
+	}
+
+}
---
+++ Fidelity/src/test/java/com/fidelity/Fidelity/FidelityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelity.Fidelity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
